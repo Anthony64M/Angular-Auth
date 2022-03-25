1f7d93c3ec47c8ff2ff856672624715e35dd7c80
Anthony64M <jymal6anthony4@gmail.com>
2022-03-25 13:36:42 +0300

    Deleted files  comited

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss'],
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
   signupForm: FormGroup;
--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from './../../shared/auth.service';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss'],
+  styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent implements OnInit {
   currentUser: Object = {};
